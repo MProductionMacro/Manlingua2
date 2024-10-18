78d7ebcf0945da860011a09ee4c4d263f7141fff
Arrick <arrickrussellg@gmail.com>
2024-10-18 14:28:40 +0700

    feat : adding VocabularyModel model for Swift Data

---
+++ Manlingua2/Utils/Services/FileManager+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  FileManager+Ext.swift
+//  Manlingua2
+//
+//  Created by Arrick Russell Adinoto on 18/10/24.
+//
+import Foundation
+
+extension FileManager {
+
+    var documentsDirectory: URL {
+        guard let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            fatalError("Unable to find user's documents directory")
+        }
+        return url
+    }
+
+    func urlInDocumentsDirectory(named: String) -> URL {
+        return documentsDirectory.appendingPathComponent(named)
+    }
+
