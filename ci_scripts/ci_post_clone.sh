1fc0a025766b94d3e7bae5823434e37e402a30c9
Paulus Michael Leang <99465357+paullmich28@users.noreply.github.com>
2024-10-29 09:24:32 +0700

    FIX: ci_post_clone (script shell fix)

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,46 +5,21 @@ if ! command -v xcodegen &> /dev/null; then
     echo "XcodeGen not found. Installing..."
     brew install xcodegen
 fi
+
+# List current directory contents
 ls .
+
 # Change to the project directory
 cd ..
-# ALL STEPS AFTER CLONE PROJECT
+
 # Generate the Xcode project using XcodeGen
 echo "Generating Xcode project..."
 xcodegen
-echo "Check file on .xcodeproj"
-ls Manlingua2.xcodeproj
-echo "Check file on project.xcworkspace"
-echo "Check file on xcshareddata"
-ls Manlingua2.xcodeproj/project.xcworkspace/xcshareddata
-# BASED ON MY EXPERIENCE xcshareddata DIRECTORY IS NOT EXIST, YOU NEED TO CREATE THE DIRECTORY
-mkdir Manlingua2.xcodeproj/project.xcworkspace/xcshareddata
-# BASED ON MY EXPERIENCE swiftpm DIRECTORY IS NOT EXIST, YOU NEED TO CREATE THE DIRECTORY
-mkdir Manlingua2.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
-# BASED ON MY EXPERIENCE Package.resolved DIRECTORY IS NOT EXIST, YOU NEED TO CREATE THE DIRECTORY
-touch Manlingua2.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
-echo "Creating Package.resolved..."
-cat <<EOL > Manlingua2.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
-# CREATE YOUR EXAMPLE DEPENDENCY HERE, ONLY FOR CREATING Package.resolved
-{
-  "originHash" : "",
-  "pins" : [
-    {
-      "identity" : "",
-      "kind" : "",
-      "location" : "",
-      "state" : {
-        "revision" : "",
-        "version" : ""
-      }
-    }
-  ],
-  "version" : 3
-}
-EOL
-# Resolve package dependencies to generate Package.resolved
+
+# Resolve package dependencies to ensure Firebase is properly added
 echo "Resolving package dependencies..."
 xcodebuild -resolvePackageDependencies -project Manlingua2.xcodeproj -scheme Manlingua2
+
 # Check if Package.resolved was created
 if [ -f "Manlingua2.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" ]; then
     echo "Package.resolved generated successfully."
