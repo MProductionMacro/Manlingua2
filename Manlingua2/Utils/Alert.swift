f632022ce41a98fe2742d3d0fa49c6026e05642e
Tcoyer <reynardoctavius84@gmail.com>
2024-10-30 18:30:41 +0700

    feat: Add Loading Page + Progress Bar + add asset

---
+++ Manlingua2/Utils/Alert.swift
@@ -0,0 +1,8 @@
+//
+//  Alert.swift
+//  Manlingua2
+//
+//  Created by Reynard Octavius Tan on 30/10/24.
+//
+
+import SwiftUI
---
+++ Manlingua2/View/LoadingPage/ProgressBarView.swift
@@ -0,0 +1,31 @@
+//
+//  ProgressBar.swift
+//  Manlingua2
+//
+//  Created by Reynard Octavius Tan on 29/10/24.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var progress: CGFloat
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(.gray.opacity(0.3))
+                    .cornerRadius(2)
+                
+                Rectangle()
+                    .foregroundColor(.green)
+                    .cornerRadius(2)
+                    .frame(width: geometry.size.width * progress)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProgressBar(progress: 0.0)
+}
