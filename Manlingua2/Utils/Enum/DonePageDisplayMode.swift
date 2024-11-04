3f76da2845641fb5a0ef06f546451c3a2fe367cc
Arrick <arrickrussellg@gmail.com>
2024-11-04 09:57:11 +0700

    style(DonePageView) : align DonePageView with figma design

---
+++ Manlingua2/Utils/Enum/DonePageDisplayMode.swift
@@ -0,0 +1,54 @@
+//
+//  DonePageDisplayMode.swift
+//  Manlingua2
+//
+//  Created by Arrick Russell Adinoto on 04/11/24.
+//
+
+import SwiftUI
+
+enum DonePageDisplayMode: Equatable, Hashable {
+    case flashcard
+    case story(id: Int, judul: String)
+}
+
+//Nanti code yang dibawah ini, semuanya akan dihapus
+enum DonePageString: String, CaseIterable{
+   case story
+   case flashCard
+   
+   var page : String{
+      switch self {
+      case .story : return "Cerita"
+      case .flashCard : return "Flashcard"
+      }
+   }
+}
+
+enum PartOfTheStory: CaseIterable{
+   case first
+   case second
+   case third
+   // add more
+   
+   var titleOfDescription: String{
+      switch self {
+      case .first : return "Bagian 1 - Perkenalan Nama"
+      case .second : return "Bagian 2 - Perkenalan Umur"
+      case .third : return "Bagian 3 - Perkenalan Pekerjaan"
+      }
+   }
+}
+
+struct DonePageModel{
+   static let title = "Selamat!"
+   static let description = "Kamu telah menyelesaikan"
+   static let returnToStoryTxt = "Balik ke cerita"
+   static let bookSymbol = "books.vertical"
+   static let returnSymbol = "return"
+   
+   static let imageName = "congratulationImage"
+   
+   static let buttonWidth : CGFloat = 361
+   static let buttonHeight : CGFloat = 64
+}
--- Manlingua2/View/Done Page/DonePageView.swift
+++ Manlingua2/View/Done Page/DonePageView.swift
@@ -7,116 +7,219 @@
 
 import SwiftUI
 
-
+//Ini nanti gak dipake, nanti dipakenya yang DonePageView2
 struct DonePageView: View {
-   var currentPage : DonePageString
-   var currentPart : PartOfTheStory
+    var currentPage : DonePageString
+    var currentPart : PartOfTheStory
    
-   @EnvironmentObject var viewModel: FlashcardViewModel
-   @EnvironmentObject var router: Router
+    @EnvironmentObject var viewModel: FlashcardViewModel
+    @EnvironmentObject var router: Router
    
-   var body: some View {
-      ZStack{
-         VStack(spacing: 10){
-            Text(DonePageModel.title)
-               .font(.system(size: 40))
-               .fontWeight(.bold)
-               .padding(12)
-            Text(DonePageModel.description)
-               .font(.system(size: 20))
-            
-            Text(currentPage == .flashCard ? "Flashcard Harian" : currentPart.titleOfDescription )
-               .font(.system(size: 20))
-               .padding(.bottom, 60)
-            
-            Image(DonePageModel.imageName)
-               .frame(width: 335, height: 225)
-               .padding(.bottom, 50)
-            
-            Button (action: {
-               router.popToRoot()
-            }){
-               HStack {
-                  Image(systemName: DonePageModel.bookSymbol)
-                  Text(DonePageModel.returnToStoryTxt)
-                     .font(.system(size: 20))
-               }
-               .foregroundStyle(Color.white)
-               .padding()
-               .frame(width: DonePageModel.buttonWidth, height: DonePageModel.buttonHeight)
-               .background(
-                  RoundedRectangle(cornerRadius: 16)
-                     .fill(Color.orange)
-               )
+    var body: some View {
+        ZStack{
+            VStack(spacing: 10){
+                Text("Selamat!")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .padding(12)
+                
+                Text("Kamu telah menyelesaikan")
+                    .font(Font.judulBiasa())
+                    .foregroundColor(.darkGrey)
+                
+                Text(currentPage == .flashCard ? "Flashcard Harian" : currentPart.titleOfDescription )
+                    .font(Font.judulBiasa())
+                    .foregroundColor(.darkGrey)
+                    .padding(.bottom, 60)
+                
+                Image(DonePageModel.imageName)
+                    .frame(width: 335, height: 225)
+                    .padding(.bottom, 50)
+                
+                
+                HStack{
+                    Button (action: {
+                        print("Button (Ulangi) Pressed")
+                        viewModel.showDonePage = false
+                        viewModel.currentIndex = 0
+                        viewModel.reshuffleCards()
+                        router.pop()
+                    }){
+                        HStack {
+                            Image(systemName: DonePageModel.returnSymbol)
+                            Text("Ulang")
+                                .font(.system(size: 20))
+                        }
+                        .foregroundStyle(Color.orange3)
+                        .padding()
+                        .frame(width: 170, height: DonePageModel.buttonHeight)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                              .stroke(Color.orange3, lineWidth: 2)
+                        )
+                    }
+                     
+                    Spacer()
+                     
+                    Button (action: {
+                        print("Button (Ulangi) Pressed")
+                        viewModel.showDonePage = false
+                        viewModel.currentIndex = 0
+                        viewModel.reshuffleCards()
+                    }){
+                        HStack {
+                           Image(systemName: "bookmark")
+                           Text("Favorit")
+                              .font(.system(size: 20))
+                           
+                        }
+                        .foregroundStyle(Color.orange3)
+                        .padding()
+                        .frame(width: 170, height: DonePageModel.buttonHeight)
+                        .overlay(
+                           RoundedRectangle(cornerRadius: 16)
+                              .stroke(Color.orange3, lineWidth: 2)
+                        )
+                    }
+                }
+                .frame(width: DonePageModel.buttonWidth)
+                 
+                Button (action: {
+                    router.popToRoot()
+                }){
+                    HStack {
+                       Image(systemName: DonePageModel.bookSymbol)
+                       Text(DonePageModel.returnToStoryTxt)
+                          .font(.system(size: 20))
+                    }
+                    .foregroundStyle(Color.white)
+                    .padding()
+                    .frame(width: DonePageModel.buttonWidth, height: DonePageModel.buttonHeight)
+                    .background(
+                       RoundedRectangle(cornerRadius: 16)
+                          .fill(Color.orange3)
+                    )
+                }
             }
-            
-            Button (action: {
-               print("Button (Ulangi) Pressed")
-               viewModel.showDonePage = false
-               viewModel.currentIndex = 0
-               viewModel.reshuffleCards()
-            }){
-               HStack {
-                  Image(systemName: DonePageModel.returnSymbol)
-                  Text("Ulangi \(currentPage.page)")
-                     .font(.system(size: 20))
-                  
-               }
-               .foregroundStyle(Color.orange)
-               .padding()
-               .frame(width: DonePageModel.buttonWidth, height: DonePageModel.buttonHeight)
-               .overlay(
-                  RoundedRectangle(cornerRadius: 16)
-                     .stroke(Color.orange, lineWidth: 2)
-               )
+        }
+    }
+}
+
+
+
+struct DonePageView2: View {
+
+    var displayMode: DonePageDisplayMode
+   
+    @EnvironmentObject var viewModel: FlashcardViewModel
+    @EnvironmentObject var router: Router
+   
+    var body: some View {
+        ZStack{
+            VStack(spacing: 10){
+                Text("Selamat!")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .padding(12)
+                
+                Text("Kamu telah menyelesaikan")
+                    .font(Font.judulBiasa())
+                    .foregroundColor(.darkGrey)
+                
+         
+                
+                if case .story(let id, let judul) = displayMode {
+                    Text("Bagian \(id) - \(judul)")
+                        .font(Font.judulBiasa())
+                        .foregroundColor(.darkGrey)
+                        .padding(.bottom, 60)
+                }
+                else{
+                    Text("Flashcard Harian")
+                        .font(Font.judulBiasa())
+                        .foregroundColor(.darkGrey)
+                        .padding(.bottom, 60)
+                }
+
+                
+                Image(DonePageModel.imageName)
+                    .frame(width: 335, height: 225)
+                    .padding(.bottom, 50)
+                
+                
+                HStack{
+                    Button (action: {
+                        print("Button (Ulangi) Pressed")
+                        viewModel.showDonePage = false
+                        viewModel.currentIndex = 0
+                        viewModel.reshuffleCards()
+                        router.pop()
+                    }){
+                        HStack {
+                            Image(systemName: DonePageModel.returnSymbol)
+                            Text("Ulang")
+                                .font(.system(size: 20))
+                        }
+                        .foregroundStyle(Color.orange3)
+                        .padding()
+                        .frame(width: 170, height: DonePageModel.buttonHeight)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                              .stroke(Color.orange3, lineWidth: 2)
+                        )
+                    }
+                     
+                    Spacer()
+                     
+                    Button (action: {
+                        print("Button (Ulangi) Pressed")
+                        viewModel.showDonePage = false
+                        viewModel.currentIndex = 0
+                        viewModel.reshuffleCards()
+                    }){
+                        HStack {
+                           Image(systemName: "bookmark")
+                           Text("Favorit")
+                              .font(.system(size: 20))
+                           
+                        }
+                        .foregroundStyle(Color.orange3)
+                        .padding()
+                        .frame(width: 170, height: DonePageModel.buttonHeight)
+                        .overlay(
+                           RoundedRectangle(cornerRadius: 16)
+                              .stroke(Color.orange3, lineWidth: 2)
+                        )
+                    }
+                }
+                .frame(width: DonePageModel.buttonWidth)
+                 
+                Button (action: {
+                    router.popToRoot()
+                }){
+                    HStack {
+                       Image(systemName: DonePageModel.bookSymbol)
+                       Text(DonePageModel.returnToStoryTxt)
+                          .font(.system(size: 20))
+                    }
+                    .foregroundStyle(Color.white)
+                    .padding()
+                    .frame(width: DonePageModel.buttonWidth, height: DonePageModel.buttonHeight)
+                    .background(
+                       RoundedRectangle(cornerRadius: 16)
+                          .fill(Color.orange3)
+                    )
+                }
             }
-            
-         }
-      }
-   }
+        }
+    }
 }
 
-/*
- #Preview {
- DonePageView()
- }
- */
-enum DonePageString: String, CaseIterable{
-   case story
-   case flashCard
-   
-   var page : String{
-      switch self {
-      case .story : return "Cerita"
-      case .flashCard : return "Flashcard"
-      }
-   }
+
+#Preview {
+    DonePageView2(displayMode: .story(id: 1, judul: "Perkenalan Nama"))
 }
 
-enum PartOfTheStory: CaseIterable{
-   case first
-   case second
-   case third
-   // add more
-   
-   var titleOfDescription: String{
-      switch self {
-      case .first : return "Bagian 1 - Perkenalan Nama"
-      case .second : return "Bagian 2 - Perkenalan Umur"
-      case .third : return "Bagian 3 - Perkenalan Pekerjaan"
-      }
-   }
-}
-
-struct DonePageModel{
-   static let title = "Selamat!"
-   static let description = "Kamu telah menyelesaikan"
-   static let returnToStoryTxt = "Balik ke cerita"
-   static let bookSymbol = "books.vertical"
-   static let returnSymbol = "return"
-   
-   static let imageName = "congratulationImage"
-   
-   static let buttonWidth : CGFloat = 361
-   static let buttonHeight : CGFloat = 64
+#Preview{
+    DonePageView(currentPage: .story, currentPart: .first)
 }
