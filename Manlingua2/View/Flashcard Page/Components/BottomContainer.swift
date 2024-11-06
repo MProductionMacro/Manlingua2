032e93f50467aaf93780cdc7bdc259a84297b0c4
Arrick <arrickrussellg@gmail.com>
2024-11-06 11:14:29 +0700

    style(BottomContainer) : align BottomContainer with figma design

--- Manlingua2/View/Flashcard Page/Components/BottomContainer.swift
+++ Manlingua2/View/Flashcard Page/Components/BottomContainer.swift
@@ -8,27 +8,44 @@
 import SwiftUI
 
 struct BottomContainerView : View {
-   @EnvironmentObject var router: Router
+    @EnvironmentObject var router: Router
    @ObservedObject var viewModel: FlashcardViewModel
    @Binding var audioController: AudioController
    
    var body : some View {
       if viewModel.showMicrophone{
 //         FlashcardMicrophoneModalityView(hanzi: viewModel.showVocabularies[viewModel.currentIndex].hanzi, responseText: $viewModel.apiResult, showMicrophone: $viewModel.showMicrophone, audioController: $audioController)
+          /*
          MicrophoneModalityView { result in
             viewModel.apiResult = result
          }
+           */
+          UserAnswerView(viewModel: viewModel)
+              .onTapGesture{
+                  withAnimation {
+                      viewModel.showMicrophone = false
+                  }
+              }
+              .animation(.easeInOut, value: viewModel.showMicrophone)
+
          
       }else if viewModel.checkResult() {
          FlashcardCorrect(showMicrophone: $viewModel.showMicrophone, audioController: $audioController){
             viewModel.performSwipeRight()
          }
+         .transition(.move(edge: .bottom))
+         //.animation(.easeInOut, value: viewModel.showMicrophone)
+
       }
       else{
          FlashcardWrong(hanzi: viewModel.showVocabularies[viewModel.currentIndex].hanzi, meaning: viewModel.showVocabularies[viewModel.currentIndex].meaning, showMicrophone: $viewModel.showMicrophone, audioController: $audioController){
             viewModel.performSwipeRight()
          }
+         .transition(.move(edge: .bottom))
+         //.animation(.easeInOut, value: viewModel.showMicrophone)
+
       }
+
    }
 }
 
