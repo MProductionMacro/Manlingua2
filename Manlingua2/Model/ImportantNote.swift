1c09431297e2bca157ee4daba480c3f32de99c94
Arrick <arrickrussellg@gmail.com>
2024-11-06 16:26:57 +0700

    feat(Kosakata JSON) : update JSON by adding hanziSentence, pinyinSentence, and meanignSentence attributes. feat(Vocabulary) : adding hanziSentence, pinyinSentence, and meanignSentence attributes

--- Manlingua2/Model/ImportantNote.swift
+++ Manlingua2/Model/ImportantNote.swift
@@ -5,9 +5,29 @@
 //  Created by Arrick Russell Adinoto on 02/11/24.
 //
 
+import Foundation
+import SwiftData
+
 struct ImportantNote: Hashable{
     let title: String
     let allowed: String
     let forbidden: String
     let caution: String
+
+}
+
+@Model
+class ImportantNoteModel{
+    var id = UUID()
+    var title: String
+    var allowed: String
+    var forbidden: String
+    var caution: String
+
+    init(_ importantNote: ImportantNote){
+        self.title = importantNote.title
+        self.allowed = importantNote.allowed
+        self.forbidden = importantNote.forbidden
+        self.caution = importantNote.caution
+    }
 }
