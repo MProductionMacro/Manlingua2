bdc8dadcbc2cd50c70be625d392182e646823137
Arrick <arrickrussellg@gmail.com>
2024-11-11 00:30:30 +0700

    feat(DictionaryViewModel) : adding localization for english and indonesian in DictionaryViewModel

--- Manlingua2/ViewModel/DictionaryViewModel.swift
+++ Manlingua2/ViewModel/DictionaryViewModel.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 class DictionaryViewModel: ObservableObject {
     @Published var vocabularies: [Vocabulary] = []
+    @Published var vocabularies_en: [Vocabulary] = []
     
     func loadVocabularies(from displayMode: DictionaryDisplayMode) {
         switch displayMode {
@@ -21,10 +22,15 @@ class DictionaryViewModel: ObservableObject {
     }
     
     private func loadFavoriteVocabularies() {
-        vocabularies = []
-        for vocab in SwiftDataServices.shared.vocabs{
-            vocabularies.append(Vocabulary(hanzi: vocab.hanzi, pinyin: vocab.pinyin, meaning: vocab.meaning, hanziSentence: vocab.hanziSentence, pinyinSentence: vocab.pinyinSentence, meaningSentence: vocab.meaningSentence))
-            //print(Vocabulary(hanzi: vocab.hanzi, pinyin: vocab.pinyin, meaning: vocab.meaning))
+        if UserDefaultSingleton.shared.language == "en"{
+            for vocab in SwiftDataServices.shared.vocabs_en{
+                vocabularies.append(Vocabulary(hanzi: vocab.hanzi, pinyin: vocab.pinyin, meaning: vocab.meaning, hanziSentence: vocab.hanziSentence, pinyinSentence: vocab.pinyinSentence, meaningSentence: vocab.meaningSentence, language: vocab.language))
+            }
+        }
+        else{
+            for vocab in SwiftDataServices.shared.vocabs{
+                vocabularies.append(Vocabulary(hanzi: vocab.hanzi, pinyin: vocab.pinyin, meaning: vocab.meaning, hanziSentence: vocab.hanziSentence, pinyinSentence: vocab.pinyinSentence, meaningSentence: vocab.meaningSentence, language: vocab.language))
+            }
         }
     }
     
@@ -34,9 +40,34 @@ class DictionaryViewModel: ObservableObject {
             loadVocabulary(from: storyId, subIndex: subIndex)
         }
     }
+        
+    public func addVocabulary(_ vocab:Vocabulary){
+        for i in 0..<vocabularies.count{
+            if vocabularies[i].hanzi == vocab.hanzi{
+                SwiftDataServices.shared.addData(vocabularies[i])
+                SwiftDataServices.shared.addData(vocabularies_en[i])
+                break
+            }
+        }
+    }
+    
+    
+    public func getVocabulary(_ displayMode: DictionaryDisplayMode) -> [Vocabulary]{
+        if displayMode == .favorite || UserDefaultSingleton.shared.language == "id"{
+            return vocabularies
+        }
+        else{
+            return vocabularies_en
+        }
+    }
     
     private func loadVocabulary(from storyId: Int, subIndex: Int) {
-        guard let url = Bundle.main.url(forResource: "Kosakata\(storyId)_\(subIndex)", withExtension: "json") else {
+        loadVocabularyId(storyId: storyId, subIndex: subIndex)
+        loadVocabularyEn(storyId: storyId, subIndex: subIndex)
+    }
+    
+    private func loadVocabularyId(storyId: Int, subIndex: Int){
+        guard let url = Bundle.main.url(forResource: "Kosakata\(storyId)_\(subIndex)_id", withExtension: "json") else {
             print("Error: File 'Kosakata\(storyId)_\(subIndex).json' not found.")
             return
         }
@@ -46,6 +77,22 @@ class DictionaryViewModel: ObservableObject {
             let decoder = JSONDecoder()
             let vocabulary = try decoder.decode([Vocabulary].self, from: data)
             vocabularies.append(contentsOf: vocabulary)
+        } catch {
+            print("Error decoding JSON from 'Kosakata\(storyId)_\(subIndex).json': \(error.localizedDescription)")
+        }
+    }
+    
+    private func loadVocabularyEn(storyId: Int, subIndex: Int){
+        guard let url = Bundle.main.url(forResource: "Kosakata\(storyId)_\(subIndex)_en", withExtension: "json") else {
+            print("Error: File 'Kosakata\(storyId)_\(subIndex).json' not found.")
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let vocabulary = try decoder.decode([Vocabulary].self, from: data)
+            vocabularies_en.append(contentsOf: vocabulary)
         } catch {
             print("Error decoding JSON from 'Kosakata\(storyId)_\(subIndex).json': \(error.localizedDescription)")
         }
