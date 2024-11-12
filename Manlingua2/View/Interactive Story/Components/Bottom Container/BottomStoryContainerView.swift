c49a271a4e966ab3fdd858b1c3519f2b22a95a0e
Arrick <arrickrussellg@gmail.com>
2024-11-12 23:50:12 +0700

    fix (BottomStoryContainerView) : fix UI error on BottomStoryContainerView

--- Manlingua2/View/Interactive Story/Components/Bottom Container/BottomStoryContainerView.swift
+++ Manlingua2/View/Interactive Story/Components/Bottom Container/BottomStoryContainerView.swift
@@ -72,7 +72,7 @@ struct BottomStoryContainerView: View {
          }
       }
       .frame(maxWidth: .infinity)
-      .background(.customLightGray)
+      .background(.cardBackground)
       .clipShape(CustomRoundedRectangle(cornerRadius: 24, corners: [.topLeft, .topRight]))
       .animation(.easeInOut(duration: 0.3), value: chatType == .question)
    }
