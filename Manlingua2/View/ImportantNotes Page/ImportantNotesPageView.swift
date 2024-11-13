542b963a337379b839518431bf67fbf16921ec10
Arrick <arrickrussellg@gmail.com>
2024-11-13 10:04:52 +0700

    fix(ImportantNotesPageView) : fixing undetected favorite notes

--- Manlingua2/View/ImportantNotes Page/ImportantNotesPageView.swift
+++ Manlingua2/View/ImportantNotes Page/ImportantNotesPageView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ImportantNotesPageView : View {
     @Environment(\.colorScheme) var colorScheme
     @EnvironmentObject var router: Router
-    @StateObject var viewModel = ImportantNotesViewModel()
+    //@StateObject var viewModel = ImportantNotesViewModel()
+    @EnvironmentObject var viewModel : ImportantNotesViewModel
     var displayMode : NoteDisplayMode
     
     var body : some View{
@@ -99,4 +100,5 @@ struct ImportantNotesPageView : View {
         ImportantNotesPageView(displayMode : .subChapter(storyId: 1, subChapterId: 1))
     }
     .environmentObject(Router())
+    .environmentObject(ImportantNotesViewModel())
 }
